a344fd5f33b05fd675f74bafe3b80a704cb8cf18
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-18 15:12:52 -0300

    Atualizei aula.js

--- aula.js
+++ aula.js
@@ -1,7 +1,14 @@
-// Tipos Primitivos
+// Tipos Primitivos de Valor
 
 let nome = 'Flávia'; // string literal
 let idade = 40; // number literal
 let estAprovado = true; // boolean
 let sobrenome ; // undefined
 let corSelecionada = null // null é usada para redefinir um valor
+
+// Tipos de Referência
+
+// function
+// array
+// object
+
