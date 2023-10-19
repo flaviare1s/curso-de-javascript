aac75e3ec3bfa9fc6fce3212b6cece6f3c7e5663
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-19 10:51:13 -0300

    Criei ex02.js

---
+++ ex02.js
@@ -0,0 +1,22 @@
+// Escreva uma função que usa dois números e retorna o maior entre eles:
+
+/*
+let valorMaior = max(11,9);
+console.log(valorMaior)
+
+function max(n1, n2) {
+    if(n1 > n2)
+        return n1;
+    else return n2
+}
+*/
+
+// Outra maneira de fazer:
+
+let valorMaior = max(11,9);
+console.log(valorMaior)
+
+function max(n1, n2) {
+    return n1 > n2 ? n1: n2;
+}
+
