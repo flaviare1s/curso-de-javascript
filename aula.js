6834bf02af1a3e5cf076f0b753f4602cfe77e268
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-01 12:14:54 -0300

    Atualizei aula.js

--- aula.js
+++ aula.js
@@ -1,14 +1,30 @@
 // Tipos Primitivos de Valor
-
+/*
 let nome = 'Flávia'; // string literal
 let idade = 40; // number literal
 let estAprovado = true; // boolean
 let sobrenome ; // undefined
 let corSelecionada = null // null é usada para redefinir um valor
-
+*/
 // Tipos de Referência
 
 // function
 // array
 // object
 
+// if...else
+
+let data = new Date()
+let hora = data.getHours()
+
+console.log(`São ${hora} horas.`)
+
+if (hora > 6 && hora < 12) {
+    console.log('Bom dia!')
+} else if (hora >= 12 && hora < 18) {
+    console.log('Boa tarde!')
+} else if (hora >= 18 < 00) {
+    console.log('Boa noite!')
+} else {
+    console.log('Boa madrugada!')
+}
