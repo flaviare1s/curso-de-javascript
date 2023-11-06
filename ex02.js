b3327ab7c13b780eba6f5802d1aab8a127fff1b4
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-06 10:00:31 -0300

    Atualizei ex02.js

--- ex02.js
+++ ex02.js
@@ -12,11 +12,25 @@ function max(n1, n2) {
 */
 
 // Outra maneira de fazer:
-
+/*
 let valorMaior = max(11,9);
 console.log(valorMaior)
 
 function max(n1, n2) {
     return n1 > n2 ? n1: n2;
+}
+*/
+
+let maiorNumero = valorMaior(30, 40)
+console.log(maiorNumero)
+
+function valorMaior(n1, n2) {
+    if (n1 > n2) {
+        return n1
+    } else if (n2 > n1) {
+        return n2
+    } else {
+        return 'Os valores digitados são iguais'
+    }
 }
 
