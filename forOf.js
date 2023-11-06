b274f612568766f0af71bff75a0109f44498ce4d
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-06 09:46:54 -0300

    Criei forOf.js

---
+++ forOf.js
@@ -0,0 +1,7 @@
+// For of mostra os valores em um array:
+
+const frutas = ['manga', 'morango', 'tangerina', 'pêra', 'laranja', 'abacaxi', 'melancia']
+
+for (fruta of frutas) {
+    console.log(fruta)
+}
