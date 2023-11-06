5e8d8df2f11a8ba50ed0a62e763bcd9d0214c392
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-06 10:13:43 -0300

    Atualizei fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,7 +4,7 @@
 // Não divisível por 3 ou 5 => entrada
 // Não for um número => não é um número
 
-
+/*
 let resultado = fizzBuzz(10);
 console.log(resultado);
 
@@ -19,4 +19,23 @@ if (entrada % 3 === 0)
 if (entrada % 5 === 0)
     return 'Buzz'
 return entrada
+}
+*/
+
+let resultado = fizzBuzz(30)
+console.log(resultado)
+
+
+function fizzBuzz(n) {
+    if (n % 3 === 0 && n % 5 === 0) {
+        return 'FizzBuzz'
+    } else if (n % 3 === 0) {
+        return 'Fizz'
+    } else if (n % 5 === 0) {
+        return 'Buzz'
+    } else if (typeof n !== 'number') {
+        return 'Não é um número!'
+    } else {
+        return n
+    }
 }
