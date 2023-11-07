54a2ea3e2f15265c7b37d741107e6ea5df5429d3
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-07 15:51:17 -0300

    Refiz o exercício 04

--- ex04.js
+++ ex04.js
@@ -1,6 +1,6 @@
 // Receber uma quantidade de valores para avaliar
 // Função exibe se cada valor é par ou ímpar
-
+/*
 exibirTipo(12)
 function exibirTipo(limite) {
    for(let i = 0; i <= limite; i++) {
@@ -11,5 +11,19 @@ function exibirTipo(limite) {
         }
     }
 }
-   
+*/
+
+function exibirTipo(limite) {
+    for (let i = 0; i <= limite; i++) {
+        if (i === 0) {
+            console.log(i, 'não é PAR nem ÍMPAR!')
+        } else if (i % 2 === 0) {
+            console.log(i,'é PAR!')
+        } else {
+            console.log(i, 'é ÍMPAR!')
+        }
+    }
+}
+
+exibirTipo(11)
   
