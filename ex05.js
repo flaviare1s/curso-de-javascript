5ae90342820d9e7765eb127522ed8d817a1e1093
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-07 16:14:39 -0300

    Refiz o exercício 05

--- ex05.js
+++ ex05.js
@@ -1,6 +1,7 @@
 // Criar um método para ler propriedades de um objeto e
 // Exibir somente as propriedades do tipo string que estão nesse objeto
 
+/*
 const filme = {
     titulo: 'Vingadores',
     ano: 2018,
@@ -13,3 +14,30 @@ function exibirPropriedades(obj) {
         if(typeof obj[prop] === 'string')
             console.log(prop,obj[prop])
 }
+*/
+
+const pessoa = {
+    nome: 'Paulo',
+    idade: 56,
+    cidade: 'Curitiba',
+    peso: 1.85
+}
+
+const filme = {
+    titulo: 'Vingadores',
+    ano: 2018,
+    diretor: 'Sei lá',
+    personagem: 'Thor'
+}
+
+function exibirValoresString(valor) {
+    for (chave in valor)
+        if ( typeof valor[chave] === 'string')
+            console.log(chave,valor[chave])
+}
+
+exibirValoresString(filme)
+
+exibirValoresString(pessoa)
+
+
