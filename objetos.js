43427e4dbb2df52caf033e940f69764d221f2070
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-12 13:44:41 -0300

    Criei objetos.js

---
+++ objetos.js
@@ -0,0 +1,17 @@
+// NATUREZA DINÂMICA DE OBEJETOS:
+
+const pessoa = {
+    nome: 'Maria',
+    idade: 21
+}
+
+// Adicionando propriedades:
+pessoa.altura = 1.72
+
+console.log(pessoa)
+
+// Deletando propriedades:
+delete pessoa.altura
+console.log(pessoa)
+
+
