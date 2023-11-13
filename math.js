6c548a9f4514a1bdc9471d34d59ebd935308a7d9
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-13 10:37:23 -0300

    Atualizei Math.js

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
 // Math.random() gera números aleatórios:
-Math.random()
+console.log(Math.random())
 
 // Math.max() diz qual é o maior valor:
-Math.max(3, 65, 22, 109, 18)
+console.log(Math.max(3, 65, 22, 109, 18))
