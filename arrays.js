71d244f569b645f735f16c9e5f7b8f54844c53c5
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-14 16:28:09 -0300

    Atualizei arrays.js

--- arrays.js
+++ arrays.js
@@ -1,15 +1,18 @@
 // ARRAYS:
-// Adicionando elementos a um array:
-let numeros = [1, 2, 3]
 
+let numeros = [1, 2, 3, 1]
+// Adicionando elementos a um array:
 // Adicionando elementos no início do array:
 numeros.unshift(0)
 console.log(numeros)
-
 // Adicionando elementos no meio do array:
 numeros.splice(1, 0, 'a')
 console.log(numeros)
-
 // Adicionando elementos no final do array:
 numeros.push(4)
 console.log(numeros)
+
+// Encontrando elementos (tipos primitivos):
+console.log(numeros.indexOf(2)) // Retorna o índice do elemento.
+console.log(numeros.indexOf(7)) // OBS: Quando o elemento não está presente no array, ele retorna o valor -1.
+console.log(numeros.lastIndexOf(1)) // Retorna o índice da última ocorrência do valor, caso o valor seja repetido.
