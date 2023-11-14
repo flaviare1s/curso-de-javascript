c379c0695dc1b9e95508b67c6b25786bdd4de054
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-14 17:14:22 -0300

    Criei arrays03.js

---
+++ arrays03.js
@@ -0,0 +1,13 @@
+// Combinando e cortando arrays:
+
+const primeiro = [1, 2, 3]
+const segundo = [4, 5, 6]
+
+// Combinando arrays:
+const combinado = primeiro.concat(segundo)
+console.log(combinado)
+
+// Dividindo arrays:
+const dividido = combinado.slice(1, 4)
+console.log(dividido)
+
