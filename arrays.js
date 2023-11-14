7e9d2cfdf113e9d2368ce509a6b06c68413e3968
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-14 16:36:19 -0300

    Atualizei arrays.js

--- arrays.js
+++ arrays.js
@@ -12,9 +12,23 @@ console.log(numeros)
 numeros.push(4)
 console.log(numeros)
 
+
 // Encontrando elementos (tipos primitivos):
 console.log(numeros.indexOf(2)) // Retorna o índice do elemento.
 console.log(numeros.indexOf(7)) // OBS: Quando o elemento não está presente no array, ele retorna o valor -1.
 console.log(numeros.lastIndexOf(1)) // Retorna o índice da última ocorrência do valor, caso o valor seja repetido.
-
 console.log(numeros.includes(2)) // Retorna true se o valor estiver presente no array.
+
+
+// Encontrando elementos (tipos de referência):
+const marcas = [
+    {id: 1, nome: 'a'},
+    {id: 2, nome: 'b'},
+    {id: 3, nome: 'c'},
+]
+//OBS.: No caso acima, o método includes não funciona.
+
+const marca = marcas.find(function(marca) {
+    return marca.nome === 'a'
+})
+console.log(marca)
