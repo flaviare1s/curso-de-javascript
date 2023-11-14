9cf2853b2ff4d552bc1a84f40b9318edf720a3ed
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-14 10:04:25 -0300

    Fiz ex12.js

---
+++ ex12.js
@@ -0,0 +1,16 @@
+// Exercício Objeto de Postagem de Blog
+
+let postagem = {
+    titulo: 'Meu Blog',
+    mensagem: `Esta é uma postagem fantasia 
+    criada para o meu blog fantasia.`,
+    autor: 'Eu',
+    visualizações: 3,
+    comentários: [
+        {autor: 'Ele', mensagem: 'Que blog pavoroso!'},
+        {autor: 'Ela', mensagem: 'Pavoroso mesmo!'},
+    ],
+    estaAoVivo: true
+}
+
+console.log(postagem)
