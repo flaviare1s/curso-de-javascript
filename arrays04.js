791694e784bed87bb1f355871c8ae10c5a023f28
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-15 10:54:17 -0300

    Criei arrays04.js

---
+++ arrays04.js
@@ -0,0 +1,14 @@
+// Utilizando JOIN:
+
+const numeros = [1, 2, 3, 4, 5]
+
+const combinado = numeros.join('.') // Adiciona um ponto entre os elementos do array
+console.log(combinado)
+
+const frase = 'Olá, bem vindo ao curso!'
+console.log(frase)
+
+const resultado = frase.split(' ') 
+console.log(resultado)
+
+console.log(resultado.join('-'))
