68dbf57b1be40f88ab23b60357cf3db6574564ef
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-15 10:35:26 -0300

    Atualizei arrays03.js

--- arrays03.js
+++ arrays03.js
@@ -7,7 +7,22 @@ const segundo = [4, 5, 6]
 const combinado = primeiro.concat(segundo)
 console.log(combinado)
 
+// Usando o operador SPREAD:
+const combinado2 = [...primeiro,...segundo]
+console.log(combinado2)
+
+// Adicionando elementos nos arrays combinados com o SPREAD:
+const combinado3 = [...primeiro, 'valorAdicionado',...segundo, 'outroValorAdicionado']
+console.log(combinado3)
+
+// Clonando arrays com o SPREAD:
+const clonado = [...combinado3]
+console.log(clonado)
+
 // Dividindo arrays:
 const dividido = combinado.slice(1, 4)
 console.log(dividido)
+
+
+
 
